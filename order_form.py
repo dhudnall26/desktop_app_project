0af4a1de84eddbe1bfc7c2093622b15b9cba03c0
Hudnall <darbyhudnall@adt.com>
2023-02-05 12:11:34 -0500

    Added back back button to allow for redo.

--- order_form.py
+++ order_form.py
@@ -17,7 +17,6 @@ class OrderForm:
         self.images = []
         self.page_count = 0
         
-        self.machine = ""
         self.paper = ""
         self.parent_sheet_size = ""
         self.parent_sheet_quantity = ""
@@ -33,17 +32,27 @@ class OrderForm:
         
         self.configurations = json.loads(open('vault/configuration.json', 'r').read())
      
+    def run_gui(self):
+        self.count_files()
 
-    def count_files(self):
-        try:
-            for path in os.listdir(self.orders_path):
-                ext = os.path.splitext(path)[-1].lower()
-                if os.path.isfile(os.path.join(self.orders_path, path)) and ext == ".pdf":
-                    self.file_count += 1
-                elif ext == ".png":
-                    os.remove(self.orders_path + path)
-        except:
-            print("No orders to process.")
+        dpg.create_context()
+        dpg.create_viewport()
+        dpg.setup_dearpygui()
+        #dpg.toggle_viewport_fullscreen()
+        
+        if self.file_count > 0:
+            self.current_file = os.listdir(self.orders_path)[0]
+        else:
+            print("No orders left to process")
+            exit()
+        
+        self.image_convert()
+        
+        self.create_window()
+
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
         
     def image_convert(self):
         self.images = convert_from_path((self.orders_path + self.current_file), fmt='png')
@@ -67,153 +76,8 @@ class OrderForm:
         self.po_number = result.split()[0]
         self.page_count = len(pdf_reader.pages)
         
-    def exit_callback(self):
-        dpg.stop_dearpygui()
-        
-    def save_callback(self):
-        self.bindery = self.bindery + self.bindery_other
-        self.set_parent_sheet_quantity()
-        string = "RUN PLAN - PURCHASE ORDER " + self.po_number + "\n\nMachine: " + self.machine + "\nPaper: " + self.paper + "\nParent Sheet Size: " + self.parent_sheet_size + "\nParent Sheet Quantity: " + self.parent_sheet_quantity + "\nPress Sheet Size: " + self.press_sheet_size + "\nPress Sheet Quantity: " + self.press_sheet_quantity + "\nImposition: " + self.imposition + "\nPress: " + self.press + "\nBindery: " + self.bindery + "\nMail Department: " + self.mail_department + "\nDisplay Graphics: " + self.display_graphics
-
-        reader = PdfReader(str(self.orders_path + self.current_file))
-        writer = PdfWriter()
-        for x in range(0, self.page_count):
-            writer.add_page(reader.pages[x])
-        writer.add_blank_page(width=612, height=792)
-
-        annotation = AnnotationBuilder.free_text(
-            string,
-            rect=(0, 396, 306, 792),
-            font="Arial",
-            bold=True,
-            italic=True,
-            font_size="20pt",
-            font_color="000000",
-            border_color="0000ff",
-            background_color="ffffff",
-        )
-        writer.add_annotation(page_number=-1, annotation=annotation)
-
-        with open((str(self.completed_orders_path + self.current_file.split('.')[0]) + "_annotated.pdf"), "wb") as fp:
-            writer.write(fp)
-        try:
-            dpg.delete_item("Order Inputs")
-            dpg.delete_item("Order Form")
-            self.create_window()
-        finally:
-            for x in range(len(self.images)):
-                dpg.delete_item("Order Image " + str(self.current_file) + str(x))
-                dpg.delete_item("Order Window " + str(self.current_file) + str(x))
-            dpg.add_text(string, parent="Order Form", pos=[600,20], tag="Order Inputs")
-            for x in range(len(self.images)):
-                os.remove(self.orders_path + (str(x) + '.png'))
-            os.replace((self.orders_path + self.current_file), (self.original_orders_path + self.current_file))
-            self.file_count -= 1
-            if self.file_count > 0:
-                self.current_file = os.listdir(self.orders_path)[0]
-                self.image_convert()
-                for x in range(len(self.images)):
-                    with dpg.drawlist(width=1000, height=1000, parent="Order Form", tag=("Order Window " + str(self.current_file) + str(x))):
-                        dpg.draw_image((str(self.current_file) + str(x) + ".png"), (0, 0), (1000, 1000), uv_min=(0, 0), uv_max=(1, 1), tag=("Order Image " + str(self.current_file) + str(x)))
-                    x+=1
-            
-            else:
-                dpg.add_text("No orders left to process", parent="Order Form", pos=[400,500])
-        
-    def set_parent_sheet_quantity(self):
-        try:
-            parent_height = float(self.parent_sheet_size.split('x')[1])
-            parent_width = float(self.parent_sheet_size.split('x')[0])
-            press_height = float(self.press_sheet_size.split('x')[1])
-            press_width = float(self.press_sheet_size.split('x')[0])
-            height = parent_height // press_height
-            width = parent_width // press_width
-            if height < width:
-                multiplier = height
-            else:
-                multiplier = width
-            self.parent_sheet_quantity = str(int(self.press_sheet_quantity) * int(multiplier))
-        except:
-            self.parent_sheet_quantity = "Please enter sheet sizes and press sent quantity and save again."
-        
-    def machine_callback(self, sender, data):
-        self.machine = data
-                         
-    def paper_callback(self, sender, data):
-        self.paper = data
-                    
-    def parent_sheet_size_callback(self, sender, data):
-        self.parent_sheet_size = data
-                             
-    def press_sheet_size_callback(self, sender, data):
-        self.press_sheet_size = data
-                         
-    def press_sheet_quantity_callback(self, sender, data):
-        self.press_sheet_quantity = data
-                        
-    def imposition_callback(self, sender, data):
-        self.imposition = data
-     
-    def press_callback(self, sender, data):
-        self.press = data
-            
-    def bindery_callback(self, sender, data, user_data):
-        if data == True:
-            self.bindery = self.bindery + user_data + ", "
-        elif data == False:
-            self.bindery.replace((user_data + ", "), "")
-            
-    def other_bindery_callback(self, sender, data, user_data):
-        self.bindery_other = data
-        
-    def mail_department_callback(self, sender, data):
-        self.mail_department = data
-             
-    def display_graphics_callback(self, sender, data):
-        self.display_graphics = data
-                 
-    def run_gui(self):
-        self.machine = ""
-        self.paper = ""
-        self.parent_sheet_size = ""
-        self.parent_sheet_quantity = ""
-        self.press_sheet_size = ""
-        self.press_sheet_quantity = ""
-        self.imposition = ""
-        self.press = ""
-        self.bindery = ""
-        self.mail_department = ""
-        self.display_graphics = ""
-   
-        self.count_files()
-
-        dpg.create_context()
-        dpg.create_viewport()
-        dpg.setup_dearpygui()
-        dpg.toggle_viewport_fullscreen()
-        
-        if self.file_count > 0:
-            self.current_file = os.listdir(self.orders_path)[0]
-        else:
-            print("No orders left to process")
-            exit()
-        
-        self.image_convert()
-        
-        self.create_window()
-
-        dpg.show_viewport()
-        dpg.start_dearpygui()
-        dpg.destroy_context()
-        
     def create_window(self):
         with dpg.window(tag="Order Form"):
-            machine = dpg.add_group(horizontal=True)
-            dpg.add_text("Machine:", parent=machine)
-            dpg.add_combo(items=(self.configurations["Machine"]), callback=self.machine_callback, parent=machine, width=200)
-            dpg.add_text("Other:", parent=machine)
-            dpg.add_input_text(callback=self.machine_callback, parent=machine, width=200)
-            
             paper = dpg.add_group(horizontal=True)
             dpg.add_text("Paper:", parent=paper)
             dpg.add_combo(items=(self.configurations["Paper"]), callback=self.paper_callback, parent=paper, width=200)
@@ -268,7 +132,7 @@ class OrderForm:
             dpg.add_input_text(callback=self.other_bindery_callback, parent=bindery, width=200)
             
             buttons = dpg.add_group(horizontal=True)
-            dpg.add_button(label="Save", callback=self.save_callback, parent=buttons)
+            dpg.add_button(label="Save", callback=self.save_confirmation_callback, parent=buttons)
             dpg.add_button(label="Exit", callback=self.exit_callback, parent=buttons)
             
             for x in range(len(self.images)):
@@ -277,4 +141,147 @@ class OrderForm:
                 x+=1
             
         dpg.set_primary_window("Order Form", True)
+        
+    def count_files(self):
+        try:
+            for path in os.listdir(self.orders_path):
+                ext = os.path.splitext(path)[-1].lower()
+                if os.path.isfile(os.path.join(self.orders_path, path)) and ext == ".pdf":
+                    self.file_count += 1
+                elif ext == ".png":
+                    os.remove(self.orders_path + path)
+        except:
+            print("No orders to process.")
+        
+    def exit_callback(self):
+        dpg.stop_dearpygui()
+        
+    def back_callback(self):
+        dpg.delete_item("Order Inputs")
+        dpg.delete_item("Order Form")
+        dpg.delete_item("Back Button")
+        self.create_window()
+        self.clear_parameters()
+        
+    def save_confirmation_callback(self):
+        self.bindery = self.bindery + self.bindery_other
+        if self.bindery[-2] == ",":
+            self.bindery = self.bindery[:-2]
+        self.set_parent_sheet_quantity()
+        string = "RUN PLAN - PURCHASE ORDER " + self.po_number + "\n\nPaper: " + self.paper + "\nParent Sheet Size: " + self.parent_sheet_size + "\nParent Sheet Quantity: " + self.parent_sheet_quantity + "\nPress Sheet Size: " + self.press_sheet_size + "\nPress Sheet Quantity: " + self.press_sheet_quantity + "\nImposition: " + self.imposition + "\nPress: " + self.press + "\nBindery: " + self.bindery + "\nMail Department: " + self.mail_department + "\nDisplay Graphics: " + self.display_graphics
+
+        with dpg.window(tag="Back Button"):
+            dpg.add_text("Press Continue to annotate PDF with the values below. Press Back to revise.", parent="Back Button")
+            dpg.add_text(string, parent="Back Button")
+            dpg.add_button(label="Continue", callback=self.save_callback, parent="Back Button")
+            dpg.add_button(label="Back", callback=self.back_callback, parent="Back Button")
+            
+    def save_callback(self):
+        string = "RUN PLAN - PURCHASE ORDER " + self.po_number + "\n\nPaper: " + self.paper + "\nParent Sheet Size: " + self.parent_sheet_size + "\nParent Sheet Quantity: " + self.parent_sheet_quantity + "\nPress Sheet Size: " + self.press_sheet_size + "\nPress Sheet Quantity: " + self.press_sheet_quantity + "\nImposition: " + self.imposition + "\nPress: " + self.press + "\nBindery: " + self.bindery + "\nMail Department: " + self.mail_department + "\nDisplay Graphics: " + self.display_graphics
+
+        reader = PdfReader(str(self.orders_path + self.current_file))
+        writer = PdfWriter()
+        for x in range(0, self.page_count):
+            writer.add_page(reader.pages[x])
+        writer.add_blank_page(width=612, height=792)
+
+        annotation = AnnotationBuilder.free_text(
+            string,
+            rect=(50, 396, 306, 792),
+            font="Arial",
+            bold=True,
+            italic=True,
+            font_size="20pt",
+            font_color="000000",
+            border_color="0000ff",
+            background_color="ffffff",
+        )
+        writer.add_annotation(page_number=-1, annotation=annotation)
+        
+        with open((str(self.completed_orders_path + self.current_file.split('.')[0]) + "_annotated.pdf"), "wb") as fp:
+            writer.write(fp)
+        try:
+            dpg.delete_item("Order Inputs")
+            dpg.delete_item("Order Form")
+            dpg.delete_item("Back Button")
+            self.create_window()
+        finally:
+            for x in range(len(self.images)):
+                dpg.delete_item("Order Image " + str(self.current_file) + str(x))
+                dpg.delete_item("Order Window " + str(self.current_file) + str(x))
+            for x in range(len(self.images)):
+                os.remove(self.orders_path + (str(x) + '.png'))
+            os.replace((self.orders_path + self.current_file), (self.original_orders_path + self.current_file))
+            self.file_count -= 1
+            if self.file_count > 0:
+                self.current_file = os.listdir(self.orders_path)[0]
+                self.image_convert()
+                for x in range(len(self.images)):
+                    with dpg.drawlist(width=1000, height=1000, parent="Order Form", tag=("Order Window " + str(self.current_file) + str(x))):
+                        dpg.draw_image((str(self.current_file) + str(x) + ".png"), (0, 0), (1000, 1000), uv_min=(0, 0), uv_max=(1, 1), tag=("Order Image " + str(self.current_file) + str(x)))
+                    x+=1
+                self.clear_parameters
+            else:
+                dpg.add_text("No orders left to process", parent="Order Form", pos=[400,500])
+                
+    def clear_parameters(self):
+        self.paper = ""
+        self.parent_sheet_size = ""
+        self.parent_sheet_quantity = ""
+        self.press_sheet_size = ""
+        self.press_sheet_quantity = ""
+        self.imposition = ""
+        self.press = ""
+        self.bindery = ""
+        self.mail_department = ""
+        self.display_graphics = ""
+        
+    def set_parent_sheet_quantity(self):
+        try:
+            parent_height = float(self.parent_sheet_size.split('x')[1])
+            parent_width = float(self.parent_sheet_size.split('x')[0])
+            press_height = float(self.press_sheet_size.split('x')[1])
+            press_width = float(self.press_sheet_size.split('x')[0])
+            height = parent_height // press_height
+            width = parent_width // press_width
+            if height < width:
+                multiplier = height
+            else:
+                multiplier = width
+            self.parent_sheet_quantity = str(int(self.press_sheet_quantity) * int(multiplier))
+        except:
+            self.parent_sheet_quantity = "Please enter sheet sizes and press sent quantity and save again."
+                   
+    def paper_callback(self, sender, data):
+        self.paper = data
+                    
+    def parent_sheet_size_callback(self, sender, data):
+        self.parent_sheet_size = data
+                             
+    def press_sheet_size_callback(self, sender, data):
+        self.press_sheet_size = data
+                         
+    def press_sheet_quantity_callback(self, sender, data):
+        self.press_sheet_quantity = data
+                        
+    def imposition_callback(self, sender, data):
+        self.imposition = data
+     
+    def press_callback(self, sender, data):
+        self.press = data
+            
+    def bindery_callback(self, sender, data, user_data):
+        if data == True:
+            self.bindery = self.bindery + user_data + ", "
+        elif data == False:
+            self.bindery.replace((user_data + ", "), "")
+            
+    def other_bindery_callback(self, sender, data, user_data):
+        self.bindery_other = data
+        
+    def mail_department_callback(self, sender, data):
+        self.mail_department = data
+             
+    def display_graphics_callback(self, sender, data):
+        self.display_graphics = data
         
